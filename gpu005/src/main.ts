929e60161ac79732d6f37816c4203a135c10421d
nghi <nghi@hotmail.com>
2021-06-07 16:37:03 +0300

    WebGPU

--- gpu005/src/main.ts
+++ gpu005/src/main.ts
@@ -1,63 +1,72 @@
+import $ from 'jquery';
 import { CheckWebGPU } from './helper';
 import { Shaders } from './shaders';
 
-const CreateTriangle = async () => {
+const CreatePrimitive = async (primitiveType = 'point-list') => { 
     const checkgpu = CheckWebGPU();
     if(checkgpu.includes('Your current browser does not support WebGPU!')){
         console.log(checkgpu);
         throw('Your current browser does not support WebGPU!');
     }
 
-    const canvas = document.getElementById('canvas-webgpu') as HTMLCanvasElement;        
+    let indexFormat = undefined;
+    if(primitiveType === 'line-strip'){
+        indexFormat = 'uint32'
+    }
+    
+    const canvas = document.getElementById('canvas-webgpu') as HTMLCanvasElement;
     const adapter = await navigator.gpu?.requestAdapter() as GPUAdapter;       
     const device = await adapter?.requestDevice() as GPUDevice;
     const context = canvas.getContext('gpupresent') as unknown as GPUCanvasContext;
+
     const swapChainFormat = 'bgra8unorm';
     const swapChain = context.configureSwapChain({
         device: device,
         format: swapChainFormat,
     });
-    
+
     const shader = Shaders();
     const pipeline = device.createRenderPipeline({
         vertex: {
-            module: device.createShaderModule({                    
+            module: device.createShaderModule({
                 code: shader.vertex
             }),
             entryPoint: "main"
         },
         fragment: {
-            module: device.createShaderModule({                    
+            module: device.createShaderModule({
                 code: shader.fragment
             }),
             entryPoint: "main",
             targets: [{
-                format: swapChainFormat
+                format: swapChainFormat as GPUTextureFormat
             }]
         },
         primitive:{
-            topology: "triangle-list"
-        } 
+            topology: primitiveType as GPUPrimitiveTopology,
+            stripIndexFormat: indexFormat as GPUIndexFormat
+        }
     });
 
     const commandEncoder = device.createCommandEncoder();
     const textureView = swapChain.getCurrentTexture().createView();
+    
     const renderPass = commandEncoder.beginRenderPass({
         colorAttachments: [{
-            view: textureView,
+            view: textureView as GPUTextureView,
             loadValue: [0.5, 0.5, 0.8, 1], //background color
             storeOp: 'store'
         }]
     });
     renderPass.setPipeline(pipeline);
-    renderPass.draw(3, 1, 0, 0);
+    renderPass.draw(6);
     renderPass.endPass();
-
+    
     device.queue.submit([commandEncoder.finish()]);
 }
 
-CreateTriangle();
-
-
-
-
+CreatePrimitive();
+$('#id-primitive').on('change', ()=>{
+    const primitiveType = $('#id-primitive').val() as string;
+    CreatePrimitive(primitiveType);
+});
