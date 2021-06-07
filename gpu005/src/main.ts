a4389e833596b1dd65dd9944815439dfcb7eac93
nghi <nghi@hotmail.com>
2021-06-07 15:44:57 +0300

    WebGPU

---
+++ gpu005/src/main.ts
@@ -0,0 +1,63 @@
+import { CheckWebGPU } from './helper';
+import { Shaders } from './shaders';
+
+const CreateTriangle = async () => {
+    const checkgpu = CheckWebGPU();
+    if(checkgpu.includes('Your current browser does not support WebGPU!')){
+        console.log(checkgpu);
+        throw('Your current browser does not support WebGPU!');
+    }
+
+    const canvas = document.getElementById('canvas-webgpu') as HTMLCanvasElement;        
+    const adapter = await navigator.gpu?.requestAdapter() as GPUAdapter;       
+    const device = await adapter?.requestDevice() as GPUDevice;
+    const context = canvas.getContext('gpupresent') as unknown as GPUCanvasContext;
+    const swapChainFormat = 'bgra8unorm';
+    const swapChain = context.configureSwapChain({
+        device: device,
+        format: swapChainFormat,
+    });
+    
+    const shader = Shaders();
+    const pipeline = device.createRenderPipeline({
+        vertex: {
+            module: device.createShaderModule({                    
+                code: shader.vertex
+            }),
+            entryPoint: "main"
+        },
+        fragment: {
+            module: device.createShaderModule({                    
+                code: shader.fragment
+            }),
+            entryPoint: "main",
+            targets: [{
+                format: swapChainFormat
+            }]
+        },
+        primitive:{
+            topology: "triangle-list"
+        } 
+    });
+
+    const commandEncoder = device.createCommandEncoder();
+    const textureView = swapChain.getCurrentTexture().createView();
+    const renderPass = commandEncoder.beginRenderPass({
+        colorAttachments: [{
+            view: textureView,
+            loadValue: [0.5, 0.5, 0.8, 1], //background color
+            storeOp: 'store'
+        }]
+    });
+    renderPass.setPipeline(pipeline);
+    renderPass.draw(3, 1, 0, 0);
+    renderPass.endPass();
+
+    device.queue.submit([commandEncoder.finish()]);
+}
+
+CreateTriangle();
+
+
+
+
